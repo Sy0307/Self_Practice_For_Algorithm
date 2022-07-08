12958f354f10d700eedd51cba42f6641f3719da3
Sy03 <1370724210@qq.com>
2022-07-08 14:41:52 +0800

    ✨ feat: ABC 258 -D

--- ABC/ABC 258/D.cpp
+++ ABC/ABC 258/D.cpp
@@ -1,8 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     ios::sync_with_stdio(false);
-    
+    using ll = long long;
+    ll n, x;
+    cin >> n >> x;
+    ll a[n], b[n];
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        cin >> b[i];
+    }
+    ll ans = LLONG_MAX;
+    ll t = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i > x)
+            break;
+        t += a[i] + b[i];
+        ans = min(ans,t+(ll)(x-i)*(b[i]));
+    }
+    cout<<ans<<endl;
+    return 0;
 }
